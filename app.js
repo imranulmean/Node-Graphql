d06d78178edec7a7b070cb5372597ecdc82efdd8
imranulmean <imranulhasan73@gmail.com>
2020-02-27 19:31:02 +0600

    updated

--- app.js
+++ app.js
@@ -4,17 +4,33 @@ const graphqlHttp= require('express-graphql');
 const {buildSchema }=require('graphql');
 const app= express();
 
+const events=[];
+
 app.use(bodyParser.json());
+
 
 app.use('/graphql',graphqlHttp({
 	schema:buildSchema(`
 
+		type Event{
+			_id:ID!
+			title:String!
+			desc:String!
+			price:Float!
+		}
+
+		input EventInput{
+			title:String!
+			desc:String!
+			price:Float!			
+		}
+
 		type RootQuery{
-			events:[String!]!
+			events:[Event!]!
 		}
 
 		type RootMutation{
-			createEvent(name:String):String
+			createEvent(eventInput:EventInput):Event
 		}
 
 		schema{
@@ -24,11 +40,19 @@ app.use('/graphql',graphqlHttp({
 	`),
 	rootValue:{
 		events:()=>{
-			return ['Marriage','Meeting','Cooking']
+			return events;
 		},
 
 		createEvent:(args)=>{
-			return JSON.stringify(args);
+			const event={
+				_id:Math.random().toString(),
+				title:args.eventInput.title,
+				desc:args.eventInput.desc,
+				price:+args.eventInput.price
+			};
+			// console.log(event);
+			events.push(event);
+			return event
 		}
 	},
 	graphiql:true
